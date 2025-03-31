ff924acb3958e42854f196d3d9fd001bed04800f
zeroeuni <pkhj286@gmail.com>
2025-03-31 11:55:30 +0900

    feat: 로그인 페이지 퍼블리싱

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,6 +4,10 @@ const Login = () => {
   return (
     <S.LoginLayout>
       <S.LoginLogo src="/BiteLogo.svg" />
+      <S.Button>
+        <S.Google src="/google.svg" />
+        <S.Text>구글 로그인</S.Text>
+      </S.Button>
     </S.LoginLayout>
   );
 };
--- src/pages/login/style.ts
+++ src/pages/login/style.ts
@@ -1,5 +1,29 @@
 import styled from "styled-components";
 
-export const LoginLayout = styled.div``;
+export const LoginLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  flex-direction: column;
+`;
 
 export const LoginLogo = styled.img``;
+
+export const Button = styled.button`
+  margin-top: 40px;
+  display: flex;
+  align-items: center;
+  padding: 17px 142px;
+  background-color: #ffffff;
+  border: 1px solid #bcbcbc;
+  border-radius: 4px;
+`;
+
+export const Google = styled.img``;
+
+export const Text = styled.p`
+  font-family: "Pretendard-Regular";
+  font-size: 20px;
+  margin: 0;
+`;
